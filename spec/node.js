d6b29e08edecbc1844b3a4321de713e65ff63f19
visionmedia <tj@vision-media.ca>
2010-01-12 12:20:05 -0800

    failuresOnly

--- spec/node.js
+++ spec/node.js
@@ -19,5 +19,5 @@ readFile = function(path) {
 
 JSpec
   .exec('spec/spec.core.js')
-  .run({ reporter: JSpec.reporters.Terminal, fixturePath: 'spec/fixtures' })
+  .run({ reporter: JSpec.reporters.Terminal, fixturePath: 'spec/fixtures', failuresOnly: true })
   .report()
