9f89ffe2c033180226df642c0db5f272c5b95e97
visionmedia <tj@vision-media.ca>
2010-02-22 14:01:07 -0800

    Updated JSpec

--- spec/node.js
+++ spec/node.js
@@ -4,19 +4,6 @@ require.paths.unshift('spec', './spec/lib/lib', 'lib')
 require('jspec')
 sass = require('sass')
 
-quit = process.exit
-print = puts
-
-readFile = function(path) {
-  var result
-  require('posix')
-    .cat(path, "utf8")
-    .addCallback(function(contents){ result = contents })
-    .addErrback(function(){ throw new Error("failed to read file `" + path + "'") })
-    .wait()
-  return result
-}
-
 JSpec
   .exec('spec/spec.core.js')
   .run({ reporter: JSpec.reporters.Terminal, fixturePath: 'spec/fixtures', failuresOnly: true })
